05034806d5f13ed4e37790186e674a4ad66afddf
Luke Covalt <ltcovalt@gmail.com>
2025-08-20 18:56:00 -0400

    limit number of enabled themes

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,4 @@
 export const defaultTheme = 'dim';
+export const enabledThemes = [
+	'autumn', 'business', 'caramellatte', 'corporate', 'dim', 'dracula', 'emerald', 'forest', 'light', 'night', 'sunset', 'winter'
+];
