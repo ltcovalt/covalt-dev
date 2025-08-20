cf79fce6e599469a921a91cdfa60a41196eb89ef
Luke Covalt <ltcovalt@gmail.com>
2025-08-20 00:29:14 -0400

    NavMenuDropdown component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from './Header.astro';
+export { default as NavMenuDropdown } from './NavMenuDropdown.astro';
 export { default as ThemeController } from './ThemeController.astro';
