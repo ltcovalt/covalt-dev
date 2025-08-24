362c8c511e4006fe5c5fb0cf62fa2aebe16a7479
Luke Covalt <ltcovalt@gmail.com>
2025-08-23 23:57:04 -0400

    split ThemeController into sub-components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Header } from './Header.astro';
 export { default as NavMenuDropdown } from './NavMenuDropdown.astro';
-export { default as ThemeController } from './ThemeController.astro';
+export { default as ThemeController } from './ThemeController/ThemeController.astro';
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -3,12 +3,12 @@
  * @param {string} theme - theme name to set as active
  */
 export const setTheme = (theme) => {
+	const currentTheme = document.documentElement.getAttribute('data-theme');
+	if (currentTheme === theme) return;
+
 	document.documentElement.setAttribute('data-theme', theme);
-	try {
-		localStorage.setItem('theme', theme);
-	} catch (ex) {
-		console.log(ex);
-	}
+	try { localStorage.setItem('theme', theme); } catch { };
+	window.dispatchEvent(new CustomEvent('theme:change', { detail: theme }));
 };
 
 /**
@@ -16,9 +16,17 @@ export const setTheme = (theme) => {
  * data-theme attribute, local storage, or the data-theme-default attribute
  */
 export const getTheme = () => {
-	const theme =
-		document.documentElement.getAttribute('data-theme') ||
-		localStorage.getItem('theme') ||
-		document.documentElement.getAttribute('data-theme-default');
+	let theme = document.documentElement.getAttribute('data-theme');
+	theme ??= (() => {
+		try { return localStorage.getItem('theme'); }
+		catch { return null; }
+	})();
+	theme ??= document.documentElement.getAttribute('data-theme-default');
 	return theme;
+};
+
+export const syncRadios = (theme = getTheme()) => {
+	document.querySelectorAll('input.theme-controller').forEach((el) => {
+		el.checked = el.value === theme;
+	});
 };
