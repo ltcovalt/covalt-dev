7a4bb0a46f148f4e843730a6fbe2f689679ecd9b
Luke Covalt <ltcovalt@gmail.com>
2025-08-24 22:56:48 -0400

    initial callout component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Callout } from './Callout/Callout.astro';
 export { default as Header } from './Header.astro';
 export { default as NavMenuDropdown } from './NavMenuDropdown.astro';
 export { default as ThemeController } from './ThemeController/ThemeController.astro';
