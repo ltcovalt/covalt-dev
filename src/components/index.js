82c46d4622b1fe5585fbbcd9badbf93abc6c055e
Luke Covalt <ltcovalt@gmail.com>
2025-09-20 16:31:02 -0400

    .

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ export { default as Alert } from './Alert.astro';
 export { default as Header } from './Header.astro';
 export { default as Link } from './Link.astro';
 export { default as NavMenuDropdown } from './NavMenuDropdown.astro';
+export { default as PostFooter } from './PostFooter.astro';
 export { default as PostPreview } from './PostPreview.astro';
 export { default as ThemeController } from './ThemeController/ThemeController.astro';
