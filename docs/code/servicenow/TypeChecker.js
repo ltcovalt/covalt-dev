761d3c8870397a1671bd804100689d3899d529ac
Luke Covalt <ltcovalt@gmail.com>
2025-11-07 01:12:07 -0500

    draft Type utils

--- docs/code/servicenow/TypeChecker.js
+++ docs/code/servicenow/TypeChecker.js
@@ -10,8 +10,17 @@ class TypeChecker {
 	constructor(name, value) {
 		this.name = name;
 		this.value = value;
-		this.messages = [];
 		this.invert = false;
+		this.checks = [];
+		this.errors = [];
+	}
+
+	get prefix() {
+		return this.invert ? "NOT " : "";
+	}
+
+	get type() {
+		return typeof this.value;
 	}
 
 	// NOTE: modifiers
@@ -21,10 +30,18 @@ class TypeChecker {
 		return this;
 	}
 
-	// TODO: is/has getters are just noop passthroughs at the moment
-	// should add only the specific methods that should be available for each
+	// TODO: getters are just noop passthroughs to make API calls grammically accurate
+	// consider limiting which predicates are available for each getter
 
 	get is() {
+		return this;
+	}
+
+	get are() {
+		return this;
+	}
+
+	get to() {
 		return this;
 	}
 
@@ -32,23 +49,51 @@ class TypeChecker {
 		return this;
 	}
 
-	// NOTE: core predicate functions
-
-	type(expected) {
-		let actual = typeof this.value;
-		let prefix = this.invert ? "NOT " : "";
-		let message = `expected typeof ${prefix}${expected}, received ${actual}`;
-		this.assert(expected === actual, message);
+	get have() {
 		return this;
 	}
 
-	// NOTE: terminal methods
+	// NOTE: core predicate functions
+
+	type(expected) {
+		let message = `${this.name}: expected typeof ${this.prefix}${expected}, received ${this.type}`;
+		this.evaluate(expected === actual, {
+			predicate: "type",
+			expected,
+			actual,
+		});
+		return this;
+	}
+
+	less(expected) {
+		let actual = typeof this.value;
+		let prefix = this.invert ? "NOT" : "";
+		let message = `${this.name}: expected ${this.value} to ${prefix}be less than ${expected}`;
+		this.evaluate(this.value < expected, {
+			predicate: "less",
+			expected,
+			actual,
+		});
+	}
+
+	lessOrEqual(expected) {
+		let actual = typeof this.value;
+		let prefix = this.invert ? "NOT" : "";
+		let message = `${this.name}: expected ${this.value} to ${prefix}be less than or equal to ${expected}`;
+		this.evaluate(this.value <= expected, {
+			predicate: "lessOrEqual",
+			expected,
+			actual,
+		});
+	}
+
+	// NOTE: terminal operation methods
 
 	/**
 	 * @returns {boolean} returns true if there were no type errors
 	 */
 	ok() {
-		return this.messages.length === 0;
+		return this.errors.length === 0;
 	}
 
 	/**
@@ -60,7 +105,7 @@ class TypeChecker {
 	result() {
 		return {
 			ok: this.ok(),
-			errors: [...this.messages],
+			errors: [...this.errors],
 		};
 	}
 
@@ -70,20 +115,47 @@ class TypeChecker {
 	 */
 	guard() {
 		if (!this.ok()) {
-			throw TypeError(this.messages.join("\n"));
+			throw TypeError(this.errors.join("\n"));
 		}
 		return this;
 	}
 
-	// NOTE: core assertion
+	// NOTE: core methods
 
-	assert(pass, message) {
+	/**
+	 * evaluates a Type.check() call chain, finalizes pass/fail,
+	 * records full check details, and logs/throws errors,
+	 * and resets the inversion flag for the next check
+	 */
+	evaluate(pass, detail) {
 		pass = this.invert ? !pass : pass;
 		if (!pass) {
-			this.messages.push(message);
+			this.errors.push(detail.message);
 		}
+
+		this.checks.push({
+			name: this.name,
+			predicate: detail.predicate,
+			expected,
+			actual,
+			pass,
+			message,
+		});
+
 		// reset the result inversion flag
 		this.invert = false;
+		return this;
+	}
+
+	/**
+	 * used to append additional checks to an existing TypeChecker
+	 * @param {string} name - name or label of the value being checked
+	 * @param {*} value - the value to be checked
+	 * @returns {this} the current TypeChecker instance
+	 */
+	check(name, value) {
+		this.name = name;
+		this.value = value;
 		return this;
 	}
 }
